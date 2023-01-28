87a1471c31dd4b399cc34bc7231f0aa42ebe669e
ImSAMazing <sam@liemburg.nl>
2023-01-28 22:51:51 +0100

    code fixjes voor env varfiabele

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import Pocketbase from 'pocketbase';
 import {writable} from 'svelte/store';
 
-export const pb = new Pocketbase(process.env.POCKETBASE);
+export const pb = new Pocketbase(import.meta.env.VITE_POCKETBASE);
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './app.css'
 import App from './App.svelte'
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+
 const app = new App({
   target: document.getElementById('app'),
 })
